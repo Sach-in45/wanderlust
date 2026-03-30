504bb7dfbabe0b3a610f5d81203b97273d097185
sachin nanvare <nanawparashuram564@gmail.com>
2026-03-30 14:49:51 +0530

    Updated filters and UI improvements

--- init/index.js
+++ init/index.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
+require("dotenv").config();
+const dbUrl = process.env.ATLASDB_URL;
 
-mongoose.connect("mongodb://127.0.0.1:27017/wanderlust")
+mongoose.connect(dbUrl)
     .then(() => {
         console.log("Connected to DB");
     })
@@ -12,7 +14,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/wanderlust")
 
 const initDB = async () => {
     await Listing.deleteMany({});
-    initData.data = initData.data.map( (obj) => ({...obj, owner: '69c4c82a480f2990e910f729'}))
     await Listing.insertMany(initData.data);
     console.log("Data was initialized");
     mongoose.connection.close();
